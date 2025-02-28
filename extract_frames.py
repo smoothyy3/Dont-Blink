4b1551c15f709b12bec05a544576f3bdcddfece4
jonas <smoothyy.3@gmail.com>
2025-02-28 20:33:12 +0100

    adding frame extraciton and basic model

---
+++ extract_frames.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+
+video_path = "/Users/jonas/Desktop/private-git/Dont-Blink/dataset/raw_vid/28.02.25/28.02.25.mp4"
+output_folder = "/Users/jonas/Desktop/private-git/Dont-Blink/dataset/raw_img/28.02.25"
+
+os.makedirs(output_folder, exist_ok=True)
+
+cap = cv2.VideoCapture(video_path)
+frame_skip = 11
+
+frame_count = 0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    if frame_count % frame_skip == 0:
+        filename = os.path.join(output_folder, f"frame_{frame_count}.jpg")
+        cv2.imwrite(filename, frame)
+
+    frame_count += 1
+
+cap.release()
+cv2.destroyAllWindows()
+
+print(f"Frames saved every {frame_skip} frames.")
